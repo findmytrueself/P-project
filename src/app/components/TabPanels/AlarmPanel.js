ec6ddd47f47b6c28d2c38538616782f956060118
findmytrueself <load1999@gmail.com>
2024-04-11 16:17:26 +0900

    update AlarmPanel

--- src/app/components/TabPanels/AlarmPanel.js
+++ src/app/components/TabPanels/AlarmPanel.js
@@ -4,38 +4,53 @@ import {
   CardContent,
   Typography,
   Stack,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
   Divider,
   TableContainer,
   Table,
+  TableBody,
+  TableCell,
   TableHead,
   TableRow,
-  TableCell,
-  TableBody,
   Paper,
 } from '@mui/material'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useBatteryContext } from '../../context/BatteryContext'
+import { clientAxiosInstance } from '../../api/axios'
+import { utcToKrTime } from '../../utils/utils'
 
 const columns = [
-  { id: '순번', label: '순번', minWidth: 100 },
-  { id: '발생시각', label: '발생시각', minWidth: 170 },
-  { id: '구분', label: '구분', minWidth: 100 },
+  { id: 'idx', label: '순번', minWidth: 100 },
+  { id: 'time', label: '발생시각', minWidth: 170 },
+  { id: 'message', label: '구분', minWidth: 100 },
 ]
 
 const AlarmPanel = () => {
-  const { batteryInfo, setBatteryInfo } = useBatteryContext()
-  const handleOnOff = (label, value) => {
-    const updatedBatteryState = batteryInfo.map((battery) => {
-      if (battery.label === label) {
-        return {
-          ...battery,
-          onOff: value,
+  const { batteryStatus } = useBatteryContext()
+  const [alarmHistory, setAlarmHistory] = useState([])
+  const [batteryNumber, setBatteryNumber] = useState('')
+
+  useEffect(() => {
+    if (batteryStatus) {
+      const { rruId, alarmSummaries } = batteryStatus
+      const getAlarmHistory = async () => {
+        try {
+          const alarmHistoryData = await clientAxiosInstance.get(
+            `/rrus/${rruId}/${alarmSummaries[batteryNumber].stringNumber}/${alarmSummaries[batteryNumber].batteryNumber}/alarms`
+          )
+          setAlarmHistory(alarmHistoryData.data)
+        } catch (e) {
+          console.error(e)
         }
       }
-      return battery
-    })
-    setBatteryInfo(updatedBatteryState)
-  }
+      getAlarmHistory()
+    }
+  }, [batteryStatus, batteryNumber])
+
   return (
     <Box sx={{ p: '16px' }}>
       <Card
@@ -69,93 +84,157 @@ const AlarmPanel = () => {
             </Typography>
           </Box>
 
-          {batteryInfo.map((battery) => (
-            <Stack
-              key={battery.batteryNumber}
-              direction="row"
-              justifyContent="space-between"
+          {batteryStatus &&
+          batteryStatus?.batteryMeasures &&
+          Object.values(batteryStatus?.alarmSummaries).length > 0 ? (
+            Object.values(batteryStatus?.alarmSummaries).map((alarm) => (
+              <Stack
+                key={alarm.batteryNumber}
+                direction="row"
+                justifyContent="space-between"
+                sx={{
+                  padding: '0 12px',
+                  borderBottom: '1px solid rgba(224, 224, 224, 1)',
+                }}
+              >
+                <Box sx={{ display: 'flex' }}>
+                  <Typography
+                    sx={{
+                      p: 1,
+                      mr: 6,
+                      fontWeight: 400,
+                      fontSize: '0.875rem',
+                      lineHeight: '2rem',
+                    }}
+                    variant="h7"
+                    component="div"
+                  >
+                    {alarm.batteryNumber}
+                  </Typography>
+                  <Divider orientation="vertical" />
+                  <Typography
+                    sx={{
+                      p: 1,
+                      fontWeight: 400,
+                      fontSize: '0.875rem',
+                      lineHeight: '2rem',
+                    }}
+                    variant="h7"
+                    component="div"
+                  >
+                    {alarm.alarmCount}
+                  </Typography>
+                </Box>
+              </Stack>
+            ))
+          ) : (
+            <Typography
               sx={{
-                padding: '0 12px',
-                borderBottom: '1px solid rgba(224, 224, 224, 1)',
+                ml: 0.5,
+                p: 1.75,
+                fontWeight: 400,
+                fontSize: '0.875rem',
+                lineHeight: '1.43',
               }}
             >
-              <Box sx={{ display: 'flex' }}>
-                <Typography
-                  sx={{
-                    p: 1,
-                    mr: 6,
-                    fontWeight: 500,
-                    fontSize: '0.875rem',
-                    lineHeight: '2rem',
-                  }}
-                  variant="h7"
-                  component="div"
-                >
-                  {battery.batteryNumber}
-                </Typography>
-                <Divider orientation="vertical" />
-                <Typography
-                  sx={{
-                    p: 1,
-                    fontWeight: 400,
-                    fontSize: '0.875rem',
-                    lineHeight: '2rem',
-                  }}
-                  variant="h7"
-                  component="div"
-                >
-                  {battery.value}
-                </Typography>
-              </Box>
-            </Stack>
-          ))}
+              RRU를 선택해주세요.
+            </Typography>
+          )}
         </CardContent>
       </Card>
 
-      {batteryInfo.map((battery) => (
-        <TableContainer sx={{ mt: 4 }} component={Paper}>
-          <Table
-            key={battery.batteryNumber}
-            stickyHeader
-            aria-label={`${battery.batteryNumber} 알람이력`}
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell colSpan={columns.length} align="left">
-                  {`${battery.batteryNumber} 알람이력`}
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    align="left"
-                    style={{ minWidth: column.minWidth }}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {battery.alarmHistory.map((history) => (
-                <TableRow
-                  hover
-                  role="checkbox"
-                  tabIndex={-1}
-                  key={battery.label}
+      <TableContainer
+        sx={{
+          mt: 4,
+          border: '1px solid rgba(224, 224, 224, 1)',
+          boxShadow: 'none',
+        }}
+        component={Paper}
+      >
+        <Table
+          key={batteryNumber}
+          stickyHeader
+          aria-label={`배터리${batteryNumber} 알람이력`}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell colSpan={columns.length} align="left">
+                <FormControl
+                  size="small"
+                  sx={{ minWidth: 150 }}
+                  disabled={!batteryStatus}
+                  // error={!batteryStatus}
                 >
+                  <InputLabel id="demo-simple-select-label">{`배터리${batteryNumber} 알람이력`}</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-required"
+                    value={batteryNumber}
+                    label={`배터리${batteryNumber} 알람이력`}
+                    onChange={(e) => setBatteryNumber(e.target.value)}
+                  >
+                    {batteryStatus &&
+                      batteryStatus.alarmSummaries &&
+                      Object.values(batteryStatus.alarmSummaries).length > 0 &&
+                      Object.values(batteryStatus.alarmSummaries).map(
+                        (battery) => (
+                          <MenuItem
+                            key={battery.batteryNumber}
+                            value={battery.batteryNumber}
+                          >{`배터리${battery.batteryNumber}`}</MenuItem>
+                        )
+                      )}
+                  </Select>
+                  <FormHelperText>
+                    {!batteryStatus ? `RRU를 선택해주세요.` : ''}
+                  </FormHelperText>
+                </FormControl>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align="left"
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {alarmHistory.length === 0 ? (
+              <Typography
+                sx={{
+                  ml: 0.5,
+                  p: 1.75,
+                  fontWeight: 400,
+                  fontSize: '0.875rem',
+                  lineHeight: '1.43',
+                }}
+              >
+                배터리를 선택해주세요.
+              </Typography>
+            ) : null}
+            {alarmHistory &&
+              alarmHistory?.list?.map((history, idx) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={idx}>
                   {columns.map((column) => (
-                    <TableCell>{history[column.id]}</TableCell>
+                    <TableCell>
+                      {!history[column.id]
+                        ? idx + 1
+                        : column.id === 'time'
+                        ? utcToKrTime(history[column.id])
+                        : history[column.id]}
+                    </TableCell>
                   ))}
-                  {console.log(history, 'history')}
                 </TableRow>
               ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   )
 }
