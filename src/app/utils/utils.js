1f23d072ac9612a774f45178c553d8e942199789
findmytrueself <load1999@gmail.com>
2024-04-11 16:18:14 +0900

    Add utcToKrTime

---
+++ src/app/utils/utils.js
@@ -0,0 +1,6 @@
+export const utcToKrTime = (utc) => {
+  const utcDate = new Date(utc)
+  const koreaOffset = 9 * 60
+  const koreaDate = new Date(utcDate.getTime() + koreaOffset * 60 * 1000)
+  return koreaDate.toLocaleString('ko-KR')
+}
