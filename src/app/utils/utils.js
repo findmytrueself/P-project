87082ec520299bf5d4c16860aa49465039ffe52e
findmytrueself <load1999@gmail.com>
2024-04-13 14:55:48 +0900

    Update date

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -3,5 +3,18 @@ export const utcToKrTime = (utc) => {
   const koreaOffset = 9 * 60
   const koreaDate = new Date(utcDate.getTime() + koreaOffset * 60 * 1000)
 
-  return koreaDate.toLocaleString('ko-KR')
+  const options = {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+    hourCycle: 'h12',
+  }
+
+  return koreaDate
+    .toLocaleString('en-US', options)
+    .replace(',', '')
+    .replace(/\//g, '.')
 }
