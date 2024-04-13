3ac5f0c4898463d736b962339c111083b5ece306
dev_hun <dev_hun@blocko.io>
2024-04-14 07:29:26 +0900

    fix bug

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -1,8 +1,13 @@
 import dayjs from "dayjs";
+import utcPlugin from "dayjs/plugin/utc"; // UTC 플러그인 추가
+import timezonePlugin from "dayjs/plugin/timezone"; // 타임존 플러그인 추가
+
+dayjs.extend(utcPlugin); // UTC 플러그인 활성화
+dayjs.extend(timezonePlugin); // 타임존 플러그인 활성화
 
 export const utcToKrTime = (utc) => {
-  const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
-  const krTime = dayjs.utc(utc).add(KR_TIME_DIFF, "millisecond");
+  const KR_TIME_ZONE = "Asia/Seoul"; // 한국 시간대
+  const krTime = dayjs.utc(utc).tz(KR_TIME_ZONE); // UTC 시간을 한국 시간으로 변환
 
   return krTime.format("YY.MM.DD h:mm A");
 };
