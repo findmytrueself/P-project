d8a78ac15c604bb82ce8b90d43e9eb8ff9f03fdd
findmytrueself <load1999@gmail.com>
2024-04-13 12:04:08 +0900

    add env

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
-import { SERVER_API_URL } from '../constant/const'
+
 /**
  * 공통 Axios 인스턴스 생성
  * 해당 인스턴스는 Client, Server Side에서 공통으로 사용할 속성을 정의하여 생성합니다
  */
+const SERVER_API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL
+
 const createInstance = (config) => {
   const defaultConfiguration = {
     // Default 값으로 설정할 config를 정리
--- src/app/constant/const.js
+++
@@ -1,1 +0,0 @@
-export const SERVER_API_URL = 'http://54.180.106.246:8080/api/v1'
