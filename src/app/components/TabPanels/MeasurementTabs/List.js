645743d37aa4668352fc39e26396874e3a3ae58e
dev_hun <dev_hun@blocko.io>
2024-04-14 15:55:34 +0900

    fix

--- src/app/components/TabPanels/MeasurementTabs/List.js
+++ src/app/components/TabPanels/MeasurementTabs/List.js
@@ -6,24 +6,24 @@ import {
   TableHead,
   TableRow,
   Paper,
-} from '@mui/material'
-import React from 'react'
+} from "@mui/material";
+import React from "react";
 
 const columns = [
-  { id: 'idx', label: '순번', minWidth: 100 },
-  { id: 'voltage', label: '전압(V)', minWidth: 100 },
-  { id: 'temperature', label: '온도(°C)', minWidth: 100 },
-  { id: 'resistance', label: '저항(mΩ)', minWidth: 100 },
-  { id: 'soc', label: 'SOC(%)', minWidth: 100 },
-  { id: 'soh', label: 'SOH(%)', minWidth: 100 },
-]
+  { id: "idx", label: "#순번", minWidth: 100 },
+  { id: "voltage", label: "전압(V)", minWidth: 100 },
+  { id: "temperature", label: "온도(°C)", minWidth: 100 },
+  { id: "resistance", label: "저항(mΩ)", minWidth: 100 },
+  { id: "soc", label: "SOC(%)", minWidth: 100 },
+  { id: "soh", label: "SOH(%)", minWidth: 100 },
+];
 
 const List = ({ batteryHistory }) => {
   return (
     <TableContainer
       sx={{
-        border: '1px solid rgba(224, 224, 224, 1)',
-        boxShadow: 'none',
+        border: "1px solid rgba(224, 224, 224, 1)",
+        boxShadow: "none",
       }}
       component={Paper}
     >
@@ -47,7 +47,9 @@ const List = ({ batteryHistory }) => {
               <TableRow hover role="checkbox" tabIndex={-1} key={idx}>
                 {columns.map((column) => (
                   <TableCell key={column.id} align="left">
-                    {!history[column.id] ? `#${idx + 1}` : history[column.id]}
+                    {!history[column.id] && history[column.id] !== 0
+                      ? idx + 1
+                      : history[column.id]}
                   </TableCell>
                 ))}
               </TableRow>
@@ -55,7 +57,7 @@ const List = ({ batteryHistory }) => {
         </TableBody>
       </Table>
     </TableContainer>
-  )
-}
+  );
+};
 
-export default List
+export default List;
