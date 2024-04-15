1ad6f9ced87d2ac77c055f64285cc4dd8acc4d33
findmytrueself <load1999@gmail.com>
2024-04-15 14:05:31 +0900

    깨지는 부분 수정

--- src/app/components/TabPanels/MeasurementTabs/Statistic.js
+++ src/app/components/TabPanels/MeasurementTabs/Statistic.js
@@ -211,31 +211,31 @@ const cardArray = [
     label: '전압(V)',
     min: { key: 'minVoltage', label: '최소값' },
     max: { key: 'maxVoltage', label: '최대값' },
-    avg: { key: 'avrVoltage', label: '평균', mr: 7.75 },
+    avg: { key: 'avrVoltage', label: '평균', mr: 7.5625 },
   },
   {
     label: '온도(°C)',
     min: { key: 'minTemperature', label: '최소값' },
     max: { key: 'maxTemperature', label: '최대값' },
-    avg: { key: 'avrTemperature', label: '평균', mr: 7.75 },
+    avg: { key: 'avrTemperature', label: '평균', mr: 7.5625 },
   },
   {
     label: '저항(mΩ)',
     min: { key: 'minResistance', label: '최소값' },
     max: { key: 'maxResistance', label: '최대값' },
-    avg: { key: 'avrResistance', label: '평균', mr: 7.75 },
+    avg: { key: 'avrResistance', label: '평균', mr: 7.5625 },
   },
 
   {
     label: 'SOC(%)',
     min: { key: 'minSoc', label: '최소값' },
     max: { key: 'maxSoc', label: '최대값' },
-    avg: { key: 'avrSoc', label: '평균', mr: 7.75 },
+    avg: { key: 'avrSoc', label: '평균', mr: 7.5625 },
   },
   {
     label: 'SOH(%)',
     min: { key: 'minSoh', label: '최소값' },
     max: { key: 'maxSoh', label: '최대값' },
-    avg: { key: 'avrSoh', label: '평균', mr: 7.75 },
+    avg: { key: 'avrSoh', label: '평균', mr: 7.5625 },
   },
 ]
